49e5def3dc1b55e23ebdad6df4eaf575c716f94c
serpro <none>
2021-07-05 19:44:18 +0300

    learning modules 11:28

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod greeting {
+	pub fn hello() {
+		println!("Hello, world!");
+	}
+
+	pub fn hello_alt() {
+		println!("What's up?");
+	}
+}
+
+use greeting::{hello, hello_alt};
+
 fn main() {
-	println!("git configured!");
+	hello();
+	hello_alt();
 }
