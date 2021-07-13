a52ad2541b6bf13a325276ce3d0df0a2201d53ec
serpro <none>
2021-07-13 10:52:24 +0300

    previous commit fix

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-mod greeting;
-use greeting::{hello as hi, hello_alt, german_hello};
+// mod greeting;
+// use greeting::{hello as hi, hello_alt, german_hello};
+
+mod helpers;
+use helpers::{get_path_usr_desktop};
 
 fn main() {
-	hi();
-	hello_alt();
-	german_hello();
+	// hi();
+	// hello_alt();
+	// german_hello();
+	let path = get_path_usr_desktop();
+	println!("{}", path);
 }
