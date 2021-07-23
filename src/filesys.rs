22cb29b29a36854845c0fc55a7694818e7aad282
serpro <none>
2021-07-23 20:24:24 +0300

    learned how to return values from modules with specific types, results and options

--- src/filesys.rs
+++ src/filesys.rs
@@ -7,10 +7,12 @@ use std::fs;
 use get_list_folder_objects::get_ls_usr_desktop;
 
 pub fn get_list_of_data() {
-	let entries = get_ls_usr_desktop();
+	let entries = get_ls_usr_desktop().unwrap();
+
+	// println!("{:?}", entries);
 
 	for value in entries {
-		let attr = fs::metadata(value);
-		println!("{:?}", attr.created().unwrap());
+		let attr = fs::metadata(value.path());
+		println!("{:?}", attr.unwrap().file_type());
 	}
 }
--- src/filesys/get_list_folder_objects.rs
+++ src/filesys/get_list_folder_objects.rs
@@ -1,18 +1,19 @@
 use super::get_path_usr_desktop;
 
 use std::{fs, io};
+// use std::path::{PathBuf};
 use get_path_usr_desktop::get_path;
+use std::fs::DirEntry;
 
-pub fn get_ls_usr_desktop() -> Result {
+pub fn get_ls_usr_desktop() -> io::Result<Vec<DirEntry>> {
 // pub fn get_ls_usr_desktop() -> Struct {
 	let d_path = get_path(); 
 
 	let entries = fs::read_dir(d_path)?
-					.map(|res| res.map(|e| e.path()))
+					.map(|res| res.map(|e| e))
 					.collect::<Result<Vec<_>, io::Error>>()?;
 
 	
-	// return entries;	
-
-	Ok(entries)
+	// return Result(entries);	
+		Ok(entries)
 }
