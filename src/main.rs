019c95fed0485f61955ed8bcf914e11e9894771b
serpro <none>
2021-07-25 19:03:15 +0300

    some garbage deleted

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-// mod greeting;
-// use greeting::{hello as hi, hello_alt, german_hello};
-
 mod helpers;
 mod filesys;
 
@@ -11,11 +8,4 @@ use filesys::get_list_of_data;
 fn main() {
 	helpers_module_loaded();
 	get_list_of_data();
-  // hi();
-  // hello_alt();
-  // german_hello();
-  // let path = get_path_usr_desktop();
-  // println!("{}", path);
-
-  // get_ls_usr_desktop().ok();
 }
